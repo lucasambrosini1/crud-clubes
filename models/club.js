cf5a25aba976df0030e1f3b9cc5218ff81f2e0e7
lucasambrosini1 <lucasjavierambrosini@gmail.com>
2022-02-14 19:40:20 -0300

    Primer version Crud Clubes

---
+++ models/club.js
@@ -0,0 +1,20 @@
+class Club {
+    constructor(id, area, name, shortName, tla, crestUrl, adress, phone, website, email, founded, clubColors, venue) {
+      this.id = id
+      this.area = area
+      this.name = name
+      this.shortName = shortName
+      this.tla = tla
+      this.crestUrl = crestUrl
+      this.adress = adress
+      this.phone = phone
+      this.website = website
+      this.email = email
+      this.founded = founded
+      this.clubColors = clubColors
+      this.venue = venue
+    }
+  }
+
+
+ module.exports = Club
