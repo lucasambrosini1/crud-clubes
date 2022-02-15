a24aa019a5538c61373704a0b6606da679e1cec8
lucasambrosini1 <lucasjavierambrosini@gmail.com>
2022-02-15 00:23:23 -0300

    Corrige errores de estilo

--- models/club.js
+++ models/club.js
@@ -1,20 +1,33 @@
 class Club {
-    constructor(id, area, name, shortName, tla, crestUrl, adress, phone, website, email, founded, clubColors, venue) {
-      this.id = id
-      this.area = area
-      this.name = name
-      this.shortName = shortName
-      this.tla = tla
-      this.crestUrl = crestUrl
-      this.adress = adress
-      this.phone = phone
-      this.website = website
-      this.email = email
-      this.founded = founded
-      this.clubColors = clubColors
-      this.venue = venue
-    }
+  constructor(
+    id,
+    area,
+    name,
+    shortName,
+    tla,
+    crestUrl,
+    adress,
+    phone,
+    website,
+    email,
+    founded,
+    clubColors,
+    venue
+  ) {
+    this.id = id;
+    this.area = area;
+    this.name = name;
+    this.shortName = shortName;
+    this.tla = tla;
+    this.crestUrl = crestUrl;
+    this.adress = adress;
+    this.phone = phone;
+    this.website = website;
+    this.email = email;
+    this.founded = founded;
+    this.clubColors = clubColors;
+    this.venue = venue;
   }
+}
 
-
- module.exports = Club
+module.exports = Club;
