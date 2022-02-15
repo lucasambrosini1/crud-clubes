4b92c5f36a9e6506e86630d65e92cf07ca9345da
lucasambrosini1 <lucasjavierambrosini@gmail.com>
2022-02-15 00:23:23 -0300

    Corrige errores de estilo

--- index.js
+++ index.js
@@ -21,76 +21,122 @@ function obtenerEquipos() {
 }
 
 app.get('/equipos', (req, res) => {
-  const equipos = obtenerEquipos()
-  
+  const equipos = obtenerEquipos();
+
   res.render('home', {
     layout: 'estructura',
     data: {
-      equipos
-    }
+      equipos,
+    },
   });
 });
 
 app.get('/equipos/:id/ver', (req, res) => {
-  const equipos =  obtenerEquipos()
-  const id =req.params.id
+  const equipos = obtenerEquipos();
+  const { id } = req.params;
   res.render('mostrar-equipo', {
     layout: 'estructura',
     data: {
-      equipo: equipos.find(equipo => equipo.id === id)
-    }
+      equipo: equipos.find((equipo) => equipo.id === id),
+    },
   });
 });
 
-
 app.get('/equipos/:id/editar', (req, res) => {
-  const equipos = obtenerEquipos()
-  const id = req.params.id
+  const equipos = obtenerEquipos();
+  const { id } = req.params;
   res.render('formulario-equipo-editar', {
     layout: 'estructura',
     data: {
-      equipo: equipos.find(equipo => equipo.id === id)
-    }
+      equipo: equipos.find((equipo) => equipo.id === id),
+    },
   });
 });
 
 app.post('/equipos/:id/editar', upload.single('crestUrl'), (req, res) => {
-  const id = req.params.id
-  const equipos = obtenerEquipos()
-  const {name, shortname, tla, adress, phone, website, email, founded, clubColors, venue,} = req.body
-  const indiceClub = equipos.findIndex(equipo => equipo.id === id)
-  const club = new Club(id,{id: 2072, name: "England"}, name, shortname, tla, `/imagenes/${req.file.filename}`, adress, phone, website, email, founded, clubColors, venue)
-  equipos[indiceClub] = club
-  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8' )
-  res.redirect("/equipos")
-  console.log(JSON.stringify(equipos), club, indiceClub)
+  const { id } = req.params;
+  const equipos = obtenerEquipos();
+  const {
+    name,
+    shortname,
+    tla,
+    adress,
+    phone,
+    website,
+    email,
+    founded,
+    clubColors,
+    venue,
+  } = req.body;
+  const indiceClub = equipos.findIndex((equipo) => equipo.id === id);
+  const club = new Club(
+    id,
+    { id: 2072, name: 'England' },
+    name,
+    shortname,
+    tla,
+    `/imagenes/${req.file.filename}`,
+    adress,
+    phone,
+    website,
+    email,
+    founded,
+    clubColors,
+    venue,
+  );
+  equipos[indiceClub] = club;
+  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8');
+  res.redirect('/equipos');
 });
 
 app.get('/equipos/agregar/', (req, res) => {
   res.render('formulario-equipo-agregar', {
-    layout: 'estructura'})
-  
+    layout: 'estructura',
+  });
 });
 
 app.post('/equipos/agregar/', upload.single('crestUrl'), (req, res) => {
-  const equipos = obtenerEquipos()
-  const {id, name, shortname, tla, adress, phone, website, email, founded, clubColors, venue} = req.body
-  const club = new Club(uuidv4(),{id: 2072, name: "England"}, name, shortname, tla, `/imagenes/${req.file.filename}`, adress, phone, website, email, founded, clubColors, venue)
-  equipos.push(club)
-  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8' );
-  res.redirect("/equipos")
+  const equipos = obtenerEquipos();
+  const {
+    name,
+    shortname,
+    tla,
+    adress,
+    phone,
+    website,
+    email,
+    founded,
+    clubColors,
+    venue,
+  } = req.body;
+  const club = new Club(
+    uuidv4(),
+    { id: 2072, name: 'England' },
+    name,
+    shortname,
+    tla,
+    `/imagenes/${req.file.filename}`,
+    adress,
+    phone,
+    website,
+    email,
+    founded,
+    clubColors,
+    venue,
+  );
+  equipos.push(club);
+  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8');
+  res.redirect('/equipos');
 });
 
 app.get('/equipos/:id/borrar', (req, res) => {
-  const equipos = obtenerEquipos()
-  const idEquipo = req.params.id
-  const indiceClub = equipos.findIndex(equipo => equipo.id === idEquipo)
-  equipos.splice(indiceClub, 1)
-  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8' );
-  res.redirect("/equipos")
+  const equipos = obtenerEquipos();
+  const idEquipo = req.params.id;
+  const indiceClub = equipos.findIndex((equipo) => equipo.id === idEquipo);
+  equipos.splice(indiceClub, 1);
+  fs.writeFileSync('./data/equipos.json', JSON.stringify(equipos), 'utf-8');
+  res.redirect('/equipos');
 });
-
 
 app.listen(PUERTO);
 console.log(`Escuchando en http://localhost:${PUERTO}`);
-
